f9093bcc7fd9903e1d4dc41adaa2fbeb51d7bd6c
Valencia <starrv2011@gmail.com>
2021-03-19 21:05:44 -0400

    worked on the UI design

--- src/main/MyWebSocket.java
+++ src/main/MyWebSocket.java
@@ -1,6 +1,9 @@
 package main;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -15,24 +18,32 @@ public class MyWebSocket
     @OnOpen
     public void onOpen(Session session) 
     {
-        System.out.println("onOpen::" + session.getId());   
-        System.out.println(session.getOpenSessions().size()+" clients connected to chat");
+        System.out.println("onOpen::" + session.getId());  
     }
     @OnClose
     public void onClose(Session session) 
     {
         System.out.println("onClose::" +  session.getId());
-        System.out.println(session.getOpenSessions().size()+" clients connected to chat");
     }
     
     @OnMessage
     public void onMessage(String message, Session session) 
     {
         System.out.println("onMessage::From=" + session.getId() + " Message=" + message);
-        try {
-            session.getBasicRemote().sendText("Hello Client " + session.getId() + "!");
-        } catch (IOException e) {
-            e.printStackTrace();
+        Set<Session> sessions=session.getOpenSessions();
+        Iterator<Session> iterator=sessions.iterator();
+        Session nextSession=null;
+        while(iterator.hasNext())
+        {
+        	nextSession=iterator.next();
+        	try 
+        	{
+        		nextSession.getBasicRemote().sendText(message);
+        	}
+        	catch(IOException e)
+        	{
+        		e.printStackTrace();
+        	}
         }
     }
     
