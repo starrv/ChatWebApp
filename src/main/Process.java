6b1f866d6761fab6b736b839d5feeed28a28d431
Valencia <starrv2011@gmail.com>
2021-03-22 16:02:25 -0400

    changes

---
+++ src/main/Process.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/process")
+public class Process extends HttpServlet
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+
+}
